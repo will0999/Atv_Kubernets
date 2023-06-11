d34ed8d02b8db0a28f08a58d484d08bd212618b2
will <56336577+will0999@users.noreply.github.com>
2023-06-11 14:55:45 -0300

    Add files via upload

---
+++ go-redis-kubernetes/quote.go
@@ -0,0 +1,35 @@
+package main
+
+type Quote struct {
+	Content      string `json:"content"`
+	Author       string `json:"author"`
+	DateAdded    string `json:"dateAdded"`
+	DateModified string `json:"dateModified"`
+}
+
+type QuoteData struct {
+	Id         string   `json:"id"`
+	Quote      string   `json:"quote"`
+	Length     string   `json:"length"`
+	Author     string   `json:"author"`
+	Tags       []string `json:"tags"`
+	Category   string   `json:"category"`
+	Date       string   `json:"date"`
+	Permalink  string   `json:"parmalink"`
+	Title      string   `json:"title"`
+	Background string   `json:"Background"`
+}
+
+type QuoteResponse struct {
+	Success  APISuccess   `json:"success"`
+	Contents QuoteContent `json:"contents"`
+}
+
+type QuoteContent struct {
+	Quotes    []QuoteData `json:"quotes"`
+	Copyright string      `json:"copyright"`
+}
+
+type APISuccess struct {
+	Total string `json:"total"`
+}
